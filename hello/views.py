fd3915fcf4a6ef479b7b133ddf86fa7689ab7ef4
Uzytkownik <wojciech.stancel@gmail.com>
2023-04-19 21:04:08 +0200

    few new views

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, world!")
+
+
+def witaj(request):
+    return HttpResponse("<h2 > Witaj, <span style='color:red'>Wojtek</span> </h2>")
+
+
+def hi(request):
+    return HttpResponse("<!DOCTYPE html><html><head><body></body></head>>/html>")
+
+
+def hi2(request):
+    return render(request, "hi.html")
